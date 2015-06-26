167c00b6e2c46ce52bb025dbcc15ef532bf9343e
Lex Herbert <lex.herbert@gmail.com>
2015-06-25 17:23:15 -0700

    fixes path issues

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['scollector']['host'] 		= '127.0.0.1'
 default['scollector']['port'] 		= 8070
 default['scollector']['bin_path'] 	= "#{node['go']['gobin']}/scollector"
-default['scollector']['conf_dir'] 	= '/etc/scollector/'
-default['scollector']['log_dir'] 	= '/var/log/scollector/'
+default['scollector']['conf_dir'] 	= '/etc/scollector'
+default['scollector']['log_dir'] 	= '/var/log/scollector'
 default['scollector']['collectors_dir'] = '/etc/scollector/collectors'
 default['scollector']['config_cookbook'] = 'scollector'
 default['scollector']['tags'] 		= {
