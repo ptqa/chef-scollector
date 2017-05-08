01f1d7e284df92ec04dd72a0a89f9988262f47e7
Taylor Monacelli <Tailor@U.Washington.edu>
2017-05-08 08:17:45 -0700

    Fix unrecognized import path "context" error for source install (#8)
    * Fix unrecognized import path "context" error for source install
    
    * Whitespace: align equals

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,12 @@
-default['scollector']['host'] 		       = '127.0.0.1'
-default['scollector']['port'] 		       = 8070
-default['scollector']['conf_dir'] 	     = '/etc/scollector'
+default['scollector']['host']            = '127.0.0.1'
+default['scollector']['port']            = 8070
+default['scollector']['conf_dir']        = '/etc/scollector'
 default['scollector']['collectors_dir']  = '/etc/scollector/collectors'
 default['scollector']['config_cookbook'] = 'scollector'
 default['scollector']['install_style']   = 'binary'
-default['scollector']['tags'] 		       = {
-  'environment' => node.chef_environment,
-  'role' => node.run_list.roles.first || 'unknown'
+default['go']['packages']                = ['bosun.org/cmd/scollector']
+default['go']['version']                 = '1.7'
+default['scollector']['tags']            = {
+    'environment' => node.chef_environment,
+    'role' => node.run_list.roles.first || 'unknown'
 }
-default['go']['packages']           	= ['bosun.org/cmd/scollector']
--- attributes/install_style.rb
+++ attributes/install_style.rb
@@ -1,3 +1,3 @@
 default['scollector']['release_url'] = 'https://github.com/bosun-monitor/bosun/releases/download'
-default['scollector']['version'] = '0.4.0'
-default['scollector']['bin_path'] = '/usr/local/bin/scollector'
+default['scollector']['version']     = '0.4.0'
+default['scollector']['bin_path']    = '/usr/local/bin/scollector'
